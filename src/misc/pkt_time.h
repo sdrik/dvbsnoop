e44afff850989ff380e93870d530a14d2e6e600e
Rainer Scherg <rasc@cvs.tuxbox.org>
2001-09-30 13:05:20 +0000

    dvbsnoop v0.7  -- Commit to CVS

---
+++ src/misc/pkt_time.h
@@ -0,0 +1,16 @@
+/*
+$Id$
+
+ -- Print Packet receive time
+ -- (c) 2001 rasc
+
+
+$Log$
+Revision 1.1  2001/09/30 13:05:20  rasc
+dvbsnoop v0.7  -- Commit to CVS
+
+
+*/
+
+
+int  out_receive_time (int verbose, OPTION *opt);
---
+++ src/sections/tsdt.c
@@ -0,0 +1,36 @@
+/*
+$Id$
+
+   -- TSDT section
+   -- Transport Stream Description Section
+   -- ISO 13818
+
+   (c) rasc
+
+
+$Log$
+Revision 1.1  2001/09/30 13:05:20  rasc
+dvbsnoop v0.7  -- Commit to CVS
+
+
+*/
+
+
+
+
+#include "dvbsnoop.h"
+#include "tsdt.h"
+
+
+
+void decode_TSDT (u_char *b, int len)
+{
+
+ out_nl (3,"TSDT-decoding....");
+ out_nl (3," Transport Stream Description Section (MPEG2) --- not implemented");
+ out_nl (3," Report, if you need it...");
+ out_NL (3);
+
+}
+
+
