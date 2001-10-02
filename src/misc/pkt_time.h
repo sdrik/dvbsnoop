feb8d664a3b6c86b8b5582648877bf9ecea138e7
Rainer Scherg <rasc@cvs.tuxbox.org>
2001-10-02 21:52:44 +0000

    - init der time_delta - PES erweitert, PES arbeitet im read() noch nicht richtig!! - muss tmbinc fragem, ob ich Mist baue, oder der Treiber (??)

--- src/misc/pkt_time.h
+++ src/misc/pkt_time.h
@@ -6,6 +6,11 @@ $Id$
 
 
 $Log$
+Revision 1.2  2001/10/02 21:52:44  rasc
+- init der time_delta
+- PES erweitert, PES arbeitet im read() noch nicht richtig!!
+- muss tmbinc fragem, ob ich Mist baue, oder der Treiber (??)
+
 Revision 1.1  2001/09/30 13:05:20  rasc
 dvbsnoop v0.7  -- Commit to CVS
 
@@ -13,4 +18,5 @@ dvbsnoop v0.7  -- Commit to CVS
 */
 
 
-int  out_receive_time (int verbose, OPTION *opt);
+void  out_receive_time (int verbose, OPTION *opt);
+void  init_receive_time (void);
