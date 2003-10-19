d22790386ebac55b79c70adcb16b3418d150a5c1
Rainer Scherg <rasc@cvs.tuxbox.org>
2003-10-19 14:00:43 +0000

    -more table decoding

--- src/version.h
+++ src/version.h
@@ -6,6 +6,9 @@ $Id$
 
 
 $Log$
+Revision 1.2  2003/10/19 14:00:43  rasc
+-more table decoding
+
 Revision 1.1  2003/10/17 19:14:40  rasc
 no message
 
@@ -20,5 +23,5 @@ no message
  * Version of dvbsnoop
  * */
 
-#define DVBSNOOP_VERSION  "1.0c"
+#define DVBSNOOP_VERSION  "1.0.1"
 
