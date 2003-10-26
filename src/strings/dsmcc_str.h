8ac99d2b277a7521d8ef4cab68d26f2fc239c822
Rainer Scherg <rasc@cvs.tuxbox.org>
2003-10-26 21:36:18 +0000

    private DSM-CC descriptor Tags started, INT-Section completed..

--- src/strings/dsmcc_str.h
+++ src/strings/dsmcc_str.h
@@ -8,6 +8,10 @@ $Id$
 
 
 $Log$
+Revision 1.3  2003/10/26 21:36:20  rasc
+private DSM-CC descriptor Tags started,
+INT-Section completed..
+
 Revision 1.2  2003/10/25 19:11:50  rasc
 no message
 
@@ -20,7 +24,7 @@ some updates to dvbsnoop...
 
 */
 
-
+char *dsmccStrDSMCCPrivateDescriptorTAG (u_int id);
 char *dsmccStrMHPOrg (u_int id);
 char *dsmccStrAction_Type (u_int id);
 char *dsmccStrProcessing_order (u_int id);
@@ -28,6 +32,7 @@ char *dsmccStrPayload_scrambling_control (u_int id);
 char *dsmccStrAddress_scrambling_control (u_int id);
 char *dsmccStrLinkage0CTable_TYPE (u_int i);
 char *dsmccStrMultiProtEncapsMACAddrRangeField (u_int i);
+char *dsmccStrPlatform_ID (u_int id);
 
 
 
