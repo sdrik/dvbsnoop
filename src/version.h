694319f9de6c8f1c3dc6fad8a683126a43cd6c0e
Rainer Scherg <rasc@cvs.tuxbox.org>
2003-12-03 21:42:08 +0000

    nothing important

--- src/version.h
+++ src/version.h
@@ -11,9 +11,15 @@ $Id$
 #ifndef __VERSION_H
 #define __VERSION_H 1
 
+/*
+ * Version set in configure.ac
+ */
+#define DVBSNOOP_VERSION     PACKAGE_VERSION
+
 
 #define DVBSNOOP_URL         "http://dvbsnoop.sourceforge.net/"
 #define DVBSNOOP_COPYRIGHT   "(c) 2001-2003  Rainer Scherg  (rasc)"
+
 
 #endif
 
