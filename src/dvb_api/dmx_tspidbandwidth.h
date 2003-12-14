6d4941da9ddce45083c8fd1378057af690b507f7
Rainer Scherg <rasc@cvs.tuxbox.org>
2003-12-14 23:42:00 +0000

    new: bandwidth usage reporting for a PID

---
+++ src/dvb_api/dmx_tspidbandwidth.h
@@ -0,0 +1,24 @@
+/*
+$Id$
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2003   Rainer.Scherg@gmx.de
+
+*/
+
+#ifndef __DMX_TSPIDBANDWIDTH_H
+#define __DMX_TSPIDBANDWIDTH_H 1
+
+
+int ts_pidbandwidth (OPTION *opt);
+
+
+#endif
+
+
