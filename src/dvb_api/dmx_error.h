b928da971521d686f778bc22fad656bf46f0d53a
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-01-02 00:00:36 +0000

    error output for buffer overflow

---
+++ src/dvb_api/dmx_error.h
@@ -0,0 +1,23 @@
+/*
+$Id$
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+*/
+
+#ifndef __DMX_ERROR_H
+#define __DMX_ERROR_H 1
+
+
+void  IO_error (u_char *str);
+
+
+#endif
+
+
