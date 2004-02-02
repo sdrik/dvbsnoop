7b6e5a460a9c03474b20260ee95106705923253d
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-02-02 23:41:23 +0000

    - output indent changed to avoid \r  (which sucks on logged output) - EBU PES data started (teletext, vps, wss, ...) - bugfix: PES synch. data stream, restructuring - some other stuff

---
+++ src/pes/pes_data_ebu.h
@@ -0,0 +1,24 @@
+/*
+$Id$
+
+   
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+
+*/
+
+#ifndef __PES_DATA_EBU_H
+#define __PES_DATA_EBU_H 1
+
+
+void PES_decodeDATA_EBU_etc (u_char *b, int len);
+
+
+#endif
+
---
+++ src/pes/pes_data_sync.h
@@ -0,0 +1,24 @@
+/*
+$Id$
+
+   
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+
+*/
+
+#ifndef __PES_DATA_SYNC_H
+#define __PES_DATA_SYNC_H 1
+
+
+void PES_decodeDATA_SYNC (u_char *b, int len);
+
+
+#endif
+
