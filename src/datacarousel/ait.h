b3562df732770256eb83af09c0e07afcd8ce5cbc
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-02-07 01:27:59 +0000

    MHP Application  Information Table some AIT descriptors

---
+++ src/datacarousel/ait.h
@@ -0,0 +1,29 @@
+/*
+$Id$
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+*/
+
+
+#ifndef __AIT_H
+#define __AIT_H 
+
+
+
+void  decode_MHP_AIT (u_char *b, int len);
+int  mhp_application_identifier (int v, u_char *b);
+
+
+
+#endif
+
+
+
