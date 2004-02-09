12cd3d1cdb2ce0aebf2a398c19ce79a4dddddb3c
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-02-09 21:24:56 +0000

    AIT descriptors minor redesign on output routines

--- src/descriptors/mhp_ait_descriptor.h
+++ src/descriptors/mhp_ait_descriptor.h
@@ -16,6 +16,10 @@ $Id$
 
 
 $Log$
+Revision 1.2  2004/02/09 21:24:58  rasc
+AIT descriptors
+minor redesign on output routines
+
 Revision 1.1  2004/02/07 01:28:01  rasc
 MHP Application  Information Table
 some AIT descriptors
@@ -36,8 +40,8 @@ int  descriptorMHP_AIT (u_char *b);
 void descriptorMHP_AIT_application (u_char *b);
 void descriptorMHP_AIT_application_name (u_char *b);
 void descriptorMHP_AIT_transport_protocol (u_char *b);
-
-
+void descriptorMHP_AIT_dvb_j_application (u_char *b);
+void descriptorMHP_AIT_dvb_j_application_location (u_char *b);
 
 
 
