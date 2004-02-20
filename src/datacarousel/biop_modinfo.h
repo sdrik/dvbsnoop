f8900eaa1fc94c28564f6816b9df35895ff0c4ec
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-02-20 22:18:37 +0000

    DII complete (hopefully) BIOP::ModuleInfo  (damned, who is spreading infos over several standards???) maybe someone give me a hint on the selector_byte info!!! some minor changes...

---
+++ src/datacarousel/biop_modinfo.h
@@ -0,0 +1,25 @@
+/*
+$Id$
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+*/
+
+
+#ifndef _BIOP_MODINFO
+#define _BIOP_MODINFO
+
+
+int biop_ModuleInfo (int v, u_char *b, u_int len);
+
+
+#endif
+
+
--- src/misc/hexprint.h
+++ src/misc/hexprint.h
@@ -19,11 +19,7 @@ $Id$
 
 void setHexPrintMode (int i);
 void printhex_buf (int verbose, u_char *buf, int len);
-
-void printhexdump_buf (int verbose, u_char *buf, int len);
-void printhexdump2_buf (int verbose, u_char *buf, int len);
-void printhexline_buf (int verbose, u_char *buf, int len);
-void printasciiline_buf (int verbose, u_char *buf, int len);
+void printasciiline_buf (int verbose, u_char *buf, int n);
 
 #endif
 
