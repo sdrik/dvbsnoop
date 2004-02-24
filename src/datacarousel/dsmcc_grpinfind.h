435ba15d622bf3df31080887574df8ca2bbf9248
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-02-24 23:14:35 +0000

    DSI:: DSMCC::GroupInfoIndication

---
+++ src/datacarousel/dsmcc_grpinfind.h
@@ -0,0 +1,25 @@
+/*
+$Id$
+
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+*/
+
+
+#ifndef __DSMCC_GRPINFIND_H
+#define __DSMCC_GRPINFIND_H
+
+int dsmcc_GroupInfoIndication (int v, u_char *b, u_int len);
+
+
+#endif
+
+
+
