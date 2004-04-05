e9b23586495371777f5fcb6347ab3cbac0c5f537
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-04-05 17:32:12 +0000

    mass typo fix adaption --> adaptation

--- src/ts/tslayer.h
+++ src/ts/tslayer.h
@@ -19,8 +19,8 @@ $Id$
 
 
 void decodeTS_buf (u_char *b, int len, int pid);
-int  ts_adaption_field (u_char *b);
-int  ts_adaption_field_extension (u_char *b);
+int  ts_adaptation_field (u_char *b);
+int  ts_adaptation_field_extension (u_char *b);
 
 
 #endif
