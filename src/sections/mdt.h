8dd02b212e864a8b88544b1e23ed948b4c573c5a
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-08-22 18:36:45 +0000

    - Bugfix: multilang service descriptor fix  (tnx to Karsten Siebert)  - New: MetaData Section  (Basic) (H.222.0 AMD1)

---
+++ src/sections/mdt.h
@@ -0,0 +1,25 @@
+/*
+$Id$
+
+ DVBSNOOP
+
+ a dvb sniffer  and mpeg2 stream analyzer tool
+ http://dvbsnoop.sourceforge.net/
+
+ (c) 2001-2004   Rainer.Scherg@gmx.de (rasc)
+
+
+
+*/
+
+
+
+#ifndef  __MDT_H
+#define  __MDT_H
+
+
+void decode_MDT (u_char *b, int len);
+
+
+#endif
+
