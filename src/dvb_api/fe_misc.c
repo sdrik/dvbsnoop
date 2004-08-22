61c2404697270ab09f67874819764c83c17a98f0
Rainer Scherg <rasc@cvs.tuxbox.org>
2004-08-22 22:11:57 +0000

    no message

--- src/dvb_api/fe_misc.c
+++ src/dvb_api/fe_misc.c
@@ -17,8 +17,12 @@ $Id$
 
 
 $Log$
+Revision 1.6  2004/08/22 22:11:57  rasc
+no message
+
 Revision 1.5  2004/08/22 22:07:48  rasc
 - Fix: nit.h got lost in Makefile
+  ifdefs for special DVB-API parameters 
 
 Revision 1.4  2004/04/01 23:02:10  rasc
 clear structures in cases these are not completly filled...
