140a13b2145f1f4e6506095a0998f03a45c5b3e5
Cédric Schieli <cschieli@gmail.com>
2016-03-17 16:05:20 +0100

    Refresh identifiers

--- src/strings/identifiers/cpSystemID.h
+++ src/strings/identifiers/cpSystemID.h
@@ -1,7 +1,6 @@
 { 0x0000, 0x0000, "DVB" },
 { 0x0001, 0x0001, "DVB" },
 { 0x0002, 0x0002, "DVB" },
-{ 0x0003, 0x00FF, "DVB" },
-{ 0x0100, 0x0100, "Digital Transmission Licensing Administrator, LLC" },
-{ 0x0101, 0x0101, "Digital Content Protection, LLC" },
+{ 0x0100, 0x0100, "DTLA, 225B Cochrane Circle, Morgan Hill, CA95037, USA. Fax: +1 408 779 9291" },
+{ 0x0101, 0x0101, "Digital Content Protection, LLC, C/O Intel Corporation, Stephen Balogh, JF2-55, 2111 NE 25th Ave, Hillsboro, OR97124, USA" },
 { 0, 0, NULL }
